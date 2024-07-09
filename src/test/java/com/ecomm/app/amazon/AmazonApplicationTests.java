fcc95798ba55fca4fd52fafa191b4d720a6a6ef1
Admin <Admin@DESKTOP-MIR7FFD>
2024-07-09 16:16:47 +0530

    First commit.

---
+++ src/test/java/com/ecomm/app/amazon/AmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.app.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
